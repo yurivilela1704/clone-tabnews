24161c4a4f7ec49687093953647364e2be5525a9
yuri <yurimarquez1704@gmail.com>
2025-10-09 23:48:16 -0300

    feat(ui): update homepage with custom text and gif

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-function Home () {
+function Home() {
   return (
     <div style={{
       minHeight: "100vh",
@@ -8,13 +8,14 @@ function Home () {
       alignItems: "center",
       background: "#fafafa"
     }}>
-      <h1 style={{ color: "#ff9800", fontSize: "3rem", marginBottom: "1rem" }}>
-        🚧 In Construction 🚧
+      <h1 style={{color: "#ff9800", fontSize: "3rem", marginBottom: "1rem"}}>
+        é betofilho japinha 🔫
       </h1>
-      <p style={{ fontSize: "1.25rem", color: "#555" }}>
-        This page is currently under construction.<br />
-        Please check back soon!
-      </p>
+      <img
+        src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExN2RoeXBzZmxzMmtwamFscTY2YWhzNjJ1NnZnMmxsOGZoMHMxM2pnbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/grdYJ5vwTaAWxnwWUq/giphy.gif"
+        alt="Under Construction"
+        style={{width: "260px", marginBottom: "1.5rem"}}
+      />
     </div>
   );
 }
